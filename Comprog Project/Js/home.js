798410830648d2fb62dbd318750c3a4ee15c5350
cabatlaomarkramon745-create <cabatlaomarkramon745@gmail.com>
2026-02-17 19:28:02 +0800

    Update home.js

--- Comprog Project/Js/home.js
+++ Comprog Project/Js/home.js
@@ -7,6 +7,14 @@
     }
   });
 
+import { auth } from "./firebase.js";
+  import { signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+  document.getElementById("logoutBtn").addEventListener("click", async () => {
+    await signOut(auth);
+    window.location.href = "login.html";
+  });
+
 document.addEventListener("DOMContentLoaded", function () {
 
   const sideMenu = document.getElementById("sideMenu");
