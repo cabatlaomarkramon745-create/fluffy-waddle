1e424648dab23b2efbe8fde98cb25cb368d53b68
cabatlaomarkramon745-create <cabatlaomarkramon745@gmail.com>
2026-02-17 23:25:20 +0800

    Update register.js

--- Comprog Project/Js/register.js
+++ Comprog Project/Js/register.js
@@ -1,4 +1,4 @@
-import { auth, db } from "../firebase.js";
+import { auth, db } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { ref, set } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
